6e68d73379b77136626c61ef72134f43250f2b41
Max Luo <luozq22@mails.tsinghua.edu.cn>
2024-10-20 12:52:43 +0800

    first commit

---
+++ filesystem_sync_client_ny5.py
@@ -0,0 +1,229 @@
+import socket
+import os
+import time
+import threading
+import hashlib
+
+BJ_DIR = '/ktt/scratch/transfer/sync_from_bj_to_ny5' 
+NY5_DIR = '/ktt/scratch/transfer/sync_from_ny5_to_bj'
+HOST = '50.248.134.234'  # JumpServer服务器地址
+PORT = 65432  # 端口号
+USER = 'server'
+TIME_INTERVAL = 10
+
+lock = threading.Lock()
+os.makedirs(BJ_DIR, exist_ok=True)
+os.makedirs(NY5_DIR, exist_ok=True)
+
+def md5(file_path):
+    """计算文件的MD5值"""
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+previous_state = {}
+for file_name in os.listdir(NY5_DIR):
+    file_path = os.path.join(NY5_DIR, file_name)
+    if os.path.isfile(file_path):
+        previous_state[file_name] = md5(file_path)
+
+
+
+def compare_dicts(old_dict, new_dict):
+    # 获取旧字典和新字典中的键
+    old_keys = set(old_dict.keys())
+    new_keys = set(new_dict.keys())
+    # 找到新增的键、删除的键和键值改变的键
+    added_keys = new_keys - old_keys
+    removed_keys = old_keys - new_keys
+    modified_keys = {key for key in old_keys & new_keys if old_dict[key] != new_dict[key]}
+    # 输出结果
+    result = {
+        "added": {key: new_dict[key] for key in added_keys},
+        "removed": {key: old_dict[key] for key in removed_keys},
+        "modified": {key: {"old": old_dict[key], "new": new_dict[key]} for key in modified_keys}
+    }
+    return result
+
+
+def monitor_folder(s):
+    """监控sync_from_ny5_to_bj文件夹变化, 一旦有变化发送文件更新信息和相关文件到JumpServer的sync_from_ny5_to_bj文件夹下"""
+    global previous_state
+
+    # if not previous_state and os.listdir(CLIENT_DIR):
+    #     for file_name in os.listdir(CLIENT_DIR):
+    #         file_path = os.path.join(CLIENT_DIR, file_name)
+    #         if os.path.isfile(file_path):
+    #             previous_state[file_name] = md5(file_path)
+    # print(previous_state)
+
+    while True:
+        current_state = {}
+        # if not os.listdir(CLIENT_DIR):
+        #     continue
+        for file_name in os.listdir(NY5_DIR):
+            file_path = os.path.join(NY5_DIR, file_name)
+            if os.path.isfile(file_path):
+                current_state[file_name] = md5(file_path)
+
+        compare_result = compare_dicts(previous_state, current_state)
+        
+        if compare_result["added"] or compare_result["removed"] or compare_result["modified"]:
+            # 发送更新信息
+            print(compare_result)
+            # lock.acquire()
+            try:
+                send_updates(compare_result, s)
+            except:
+                print("发送更新信息时客户端连接服务器失败，可能是服务器掉线或端口被占用, 立即返回主线程尝试重连，若不成功则每10秒重连一次")
+                # lock.release()
+                return
+            # lock.release()
+            previous_state = current_state
+        time.sleep(TIME_INTERVAL)  # 每隔一段时间检查一次文件夹
+
+
+def send_updates(result:dict, s):
+    """将文件夹更新信息、相关文件发送到JumpServer的sync_from_ny5_to_bj文件夹下"""
+    if result["added"]:
+        for filename in list(result["added"].keys()):
+            file_path = os.path.join(NY5_DIR, filename)
+            with open(file_path, 'rb') as f:
+                file_data = f.read()
+            message = f"ADD|{os.path.basename(file_path)}|{len(file_data)}EOF".encode()
+            s.sendall(message)
+            exit_code = os.system(f"scp {file_path} {USER}@{HOST}:{NY5_DIR}") #主动从纽约向JumpServer发起scp传输文件
+            if exit_code == 0:
+                s.sendall("ADD Transmission EndEOF".encode())
+                print(f"纽约客户端操作：从{NY5_DIR}添加文件{filename}，已被同步传输")
+            else:
+                print(f"纽约客户端从{NY5_DIR}传送新添加的文件{filename}失败，等待重连...")
+                raise
+    elif result["modified"]:
+        for filename in list(result["modified"].keys()):
+            file_path = os.path.join(NY5_DIR, filename)
+            with open(file_path, 'rb') as f:
+                file_data = f.read()
+            message = f"MODIFY|{os.path.basename(file_path)}|{len(file_data)}EOF".encode()
+            s.sendall(message)
+            exit_code = os.system(f"scp {file_path} {USER}@{HOST}:{NY5_DIR}") #主动从纽约向JumpServer发起scp传输文件
+            if exit_code == 0:
+                s.sendall("MODIFY Transmission EndEOF".encode())
+                print(f"纽约客户端操作：从{NY5_DIR}修改文件{filename}，已被同步传输")
+            else:
+                print(f"纽约客户端从{NY5_DIR}传送新修改的文件{filename}失败，等待重连")
+                raise
+    elif result["removed"]:
+        for filename in list(result["removed"].keys()):
+            message = f"REMOVE|{filename}|0EOF".encode()
+            s.sendall(message)
+            print(f"纽约客户端操作：在{NY5_DIR}删除文件信息已被同步传输")
+
+
+def receive_updates(s, addr):
+    """接收JumpServer发来的sync_from_bj_to_ny5文件夹更新信息, 并根据更新信息发起scp请求, 更新sync_from_bj_to_ny5文件夹"""
+    while True:
+        try:
+            data = s.recv(1024)
+        except:
+            print("接收更新信息时客户端连接服务器失败，可能是服务器掉线或端口被占用, 下次发送更新信息失败后尝试重连，若不成功则每10秒重连一次")
+            return
+        if not data:
+            continue
+        list_data = [d for d in data.decode().split('EOF') if d]
+        # lock.acquire()
+        """根据接收到的更新信息更新本地文件夹"""
+        for data in list_data: 
+            if data == 'check_conn':
+                continue     
+            command, filename, file_size = data.split('|') 
+            file_size = int(file_size)
+            file_path = os.path.join(BJ_DIR, filename)
+
+            if command == "ADD":
+                exit_code = os.system(f'scp {USER}@{HOST}:{os.path.join(BJ_DIR, filename)} {os.path.join(BJ_DIR, filename)}') 
+                if exit_code == 0:
+                    print(f"纽约客户端{BJ_DIR}文件夹下文件{filename}已被同步添加.")
+                else:
+                    print(f"纽约客户端获取{filename}失败，可能连接断开，尝试修改{NY5_DIR}下文件，触发重连...")
+                    return
+
+            elif command == "MODIFY":
+                exit_code = os.system(f'scp {USER}@{HOST}:{os.path.join(BJ_DIR, filename)} {os.path.join(BJ_DIR, filename)}')
+                if exit_code == 0:
+                    print(f"纽约客户端{BJ_DIR}文件夹下文件{filename}已被同步添加.")
+                else:
+                    print(f"纽约客户端获取{filename}失败，等待重连...")
+                    return
+
+            elif command == "REMOVE":
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+                    print(f"纽约客户端{BJ_DIR}下文件{filename}已被同步删除")
+
+        # lock.release()
+
+init_send = False
+def begin_with_sendall(local_dir=BJ_DIR):
+    global init_send
+    exit_code = os.system(f"rsync -av --delete {local_dir} {USER}@{HOST}:{os.path.dirname(local_dir)}/")
+    if exit_code == 0:
+        client_socket.sendall("Client Rsync Send Over!".encode())
+        init_send = True
+        print(f"纽约客户端向服务器{NY5_DIR}初始化文件传输结束")
+    else:
+        client_socket.sendall("Client Rsync ERROR!".encode())   
+        print(f"纽约客户端向服务器{NY5_DIR}初始化文件传输失败，尝试重连...") 
+
+init_recv = False
+def begin_with_recvall(local_dir=NY5_DIR):
+    global init_recv
+    exit_code = os.system(f"rsync -av --delete {USER}@{HOST}:{local_dir} {os.path.dirname(local_dir)}/")
+    if exit_code == 0:
+        client_socket.sendall("Client Rsync Recv Over!".encode())
+        init_recv = True
+        print(f"纽约客户端接收服务器{BJ_DIR}初始化文件传输结束")
+    else:
+        client_socket.sendall("Client Rsync ERROR!".encode())
+        print(f"纽约客户端接收服务器{BJ_DIR}初始化文件传输失败，尝试重连...")
+
+if __name__ == "__main__":
+    # 启动两个线程：监控线程和接收线程
+    while True:
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((HOST, PORT))
+            print("客户端成功连接服务器")
+
+            client_socket.sendall('New York'.encode())
+            time.sleep(1)
+
+            t0 = threading.Thread(target=begin_with_sendall, args=[NY5_DIR])
+            t0_ = threading.Thread(target=begin_with_recvall, args=[BJ_DIR])
+            t1 = threading.Thread(target=receive_updates, args=[client_socket, HOST])
+            t2 = threading.Thread(target=monitor_folder, args=[client_socket])
+            
+            
+            t0.start()
+            t0_.start()
+            t0.join()
+            t0_.join()
+
+            if not (init_send and init_recv): #初始化失败
+                continue
+            
+            print(f"纽约客户端开始接收来自{HOST}:{BJ_DIR}文件更新信息")
+            t1.start() #先接收JumpServer上BJ文件夹中更新的文件信息
+            time.sleep(5) 
+            print(f"纽约客户端开始监控{NY5_DIR}文件更新信息，并向{HOST}发送")
+            t2.start()
+
+            # t1.join()
+            t2.join()
+        
+        except:
+            print(f"纽约客户端连接服务器{HOST}失败, 10秒后尝试重连...")
+            time.sleep(10)
+            continue
---
+++ filesystem_sync_server_jumpserver.py
@@ -0,0 +1,243 @@
+import socket
+import os
+import time
+import threading
+import hashlib
+import argparse
+
+BJ_DIR = '/home/mluo/sync_from_bj_to_ny5'  # 服务器的文件夹路径
+NY5_DIR = '/home/mluo/sync_from_ny5_to_bj'
+TIME_INTERVAL = 10
+HOST = '0.0.0.0'  # 端口号
+PORT = 65432  # 端口号
+
+lock = threading.Lock()
+bj_previous_state = {}
+ny5_previous_state = {}
+
+
+def md5(file_path):
+    """计算文件的MD5值"""
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def compare_dicts(old_dict, new_dict):
+    # 获取旧字典和新字典中的键
+    old_keys = set(old_dict.keys())
+    new_keys = set(new_dict.keys())
+    # 找到新增的键、删除的键和键值改变的键
+    added_keys = new_keys - old_keys
+    removed_keys = old_keys - new_keys
+    modified_keys = {key for key in old_keys & new_keys if old_dict[key] != new_dict[key]}
+    # 输出结果
+    result = {
+        "added": {key: new_dict[key] for key in added_keys},
+        "removed": {key: old_dict[key] for key in removed_keys},
+        "modified": {key: {"old": old_dict[key], "new": new_dict[key]} for key in modified_keys}
+    }
+    return result
+
+def monitor_folder(conn, addr):
+    """监控指定文件夹变化"""
+    global bj_previous_state
+    global ny5_previous_state
+
+    if addr == '北京':
+        ny5_previous_state = {}
+        monitor_dir = NY5_DIR 
+        while True: #开始监控从纽约要往北京发送的文件
+            ny5_current_state = {}
+            # if not os.listdir(SERVER_DIR):
+            #     continue
+            for file_name in os.listdir(monitor_dir):
+                file_path = os.path.join(monitor_dir, file_name)
+                if os.path.isfile(file_path):
+                    ny5_current_state[file_name] = md5(file_path)
+            
+            compare_result = compare_dicts(ny5_previous_state, ny5_current_state) #sync_from_bj_to_ny5状态检测
+            
+            if compare_result["added"] or compare_result["removed"] or compare_result["modified"]:
+                print(f"{monitor_dir}下状态{compare_result}")
+                # 发送更新信息
+                lock.acquire()            
+                try:
+                    send_updates(compare_result, conn, monitor_dir, addr)
+                except:
+                    print(f"来自{addr}的客户端掉线，若长时间未恢复则可能死机，请检查")
+                    lock.release()
+                    return
+                lock.release()
+                ny5_previous_state = ny5_current_state
+            time.sleep(TIME_INTERVAL)  # 每隔一段时间检查一次文件夹
+
+    elif addr == '纽约':
+        bj_previous_state = {}
+        monitor_dir = BJ_DIR
+        while True: #开始监控从北京要往纽约发送的文件
+            bj_current_state = {}
+            # if not os.listdir(SERVER_DIR):
+            #     continue
+            for file_name in os.listdir(monitor_dir):
+                file_path = os.path.join(monitor_dir, file_name)
+                if os.path.isfile(file_path):
+                    bj_current_state[file_name] = md5(file_path)
+            
+            compare_result = compare_dicts(bj_previous_state, bj_current_state) #sync_from_ny5_to_bj状态检测
+            
+            if compare_result["added"] or compare_result["removed"] or compare_result["modified"]:
+                print(f"{monitor_dir}下状态{compare_result}")
+                # 发送更新信息            
+                lock.acquire()
+                try:
+                    send_updates(compare_result, conn, monitor_dir, addr)
+                except:
+                    print(f"来自{addr}的客户端掉线，若长时间未恢复则可能死机，请检查")
+                    lock.release()
+                    return
+                lock.release()
+                bj_previous_state = bj_current_state
+            time.sleep(TIME_INTERVAL)  # 每隔一段时间检查一次文件夹
+    else:
+        print("客户端IP不符合要求, 必须为北京或纽约的主机, 否则不接受客户端的TCP连接请求")
+        return
+
+def send_updates(result:dict, conn, monitor_dir, addr):
+    """将指定的监控的文件夹更新信息发送到客户端"""
+    if result["added"]:
+        for filename in list(result["added"].keys()):
+            file_path = os.path.join(monitor_dir, filename)
+            with open(file_path, 'rb') as f:
+                file_data = f.read()
+            message = f"ADD|{os.path.basename(file_path)}|{len(file_data)}EOF".encode()
+            conn.sendall(message)
+            print(f"{monitor_dir}添加{filename}的文件信息已发送到{addr}，等待客户端同步...")
+    elif result["modified"]:
+        for filename in list(result["modified"].keys()):
+            file_path = os.path.join(monitor_dir, filename)
+            with open(file_path, 'rb') as f:
+                file_data = f.read()
+            message = f"MODIFY|{os.path.basename(file_path)}|{len(file_data)}EOF".encode()
+            conn.sendall(message)
+            print(f"{monitor_dir}修改{filename}的文件信息已发送到{addr}，等待客户端同步...")
+    elif result["removed"]:
+        for filename in list(result["removed"].keys()):
+            message = f"REMOVE|{filename}|0EOF".encode()
+            conn.sendall(message)
+            print(f"{monitor_dir}删除{filename}的文件信息已发送到{addr}，等待客户端同步...")
+
+def receive_updates(conn, addr):
+    """接收客户端发来的文件夹更新信息, 并根据BJ还是NYU采取不同的逻辑"""
+    while True:
+        try:
+            data = conn.recv(1024)
+        except:
+            print(f"来自{addr}的客户端掉线，若长时间未恢复则可能死机，请检查")
+            return
+        if not data:
+            continue
+        list_data = [d for d in data.decode().split('EOF') if d]
+        if not list_data:
+            continue
+        lock.acquire()
+        """根据接收到的更新信息更新本地文件夹"""
+        for data in list_data: 
+            if data == "ADD Transmission End":
+                print(f"来自{addr}的添加文件{filename}已被同步添加.") 
+            elif data == "MODIFY Transmission End":
+                print(f"来自{addr}的修改文件{filename}已被同步修改.") 
+            else:        
+                command, filename, file_size = data.split('|') #这里file_data只有开头一部分
+                file_size = int(file_size)
+                if addr == '北京':
+                    file_path = os.path.join(BJ_DIR, filename)
+                elif addr == '纽约':
+                    file_path = os.path.join(NY5_DIR, filename)
+
+                if command == "REMOVE":
+                    if os.path.exists(file_path):
+                        os.remove(file_path)
+                        print(f"{os.path.basename(file_path)}下文件{filename}已被同步删除")
+
+        lock.release()
+
+
+def check_conn(conn, addr):
+    time.sleep(5)
+    while True:
+        try:
+            conn.sendall(b'check_connEOF')
+            time.sleep(5)
+        except:
+            print(f"来自{addr}的客户端掉线，若长时间未恢复则可能死机，请检查")
+            return
+
+init_rsync = False
+def begin_rsync(conn):
+    global init_rsync
+    while True:
+        try:
+            data = conn.recv(1024).decode()
+            if data == "Client Rsync Recv Over!":
+                init_rsync = True
+                return 
+            if data == "Client Rsync ERROR!":
+                print("客户端初始化文件同步失败，等待重连...")
+                return
+        except:
+            return 
+
+if __name__ == "__main__":
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
+    print(f"{HOST}服务器在{PORT}端口等待连接...")
+
+    while True:
+        conn, addr = server_socket.accept()
+        print(f"来自{addr}客户端的连接建立")
+
+        data = conn.recv(1024).decode()
+        if data == 'Bei Jing':
+            addr = '北京'
+            print("客户端同步请求来自北京")
+        elif data == 'New York':
+            addr = '纽约'
+            print("客户端同步请求来自纽约")
+
+        # 启动两个线程：监控线程和接收线程
+        
+        t0 = threading.Thread(target=begin_rsync, args=[conn])
+        t1 = threading.Thread(target=monitor_folder, args=[conn, addr])
+        t2 = threading.Thread(target=receive_updates, args=[conn, addr]) #注意先后顺序
+        t3 = threading.Thread(target=check_conn, args=[conn, addr])
+
+        lock.acquire()
+        t0.start()
+        t0.join()  # 必须先跟客户端的本地文件夹同步再进行后面的工作
+        if not init_rsync:
+            lock.release()
+            print(f"来自{addr}的客户端初始化文件同步失败，可能是客户端掉线，若长时间未恢复则可能死机，请检查")
+            continue
+        print(f"与{addr}客户端文件初始化同步结束")
+        lock.release()
+
+        if addr == 'Bei Jing':
+            ny5_previous_state = {}
+            print(f"开始监控{NY5_DIR},向{addr}客户端发送文件夹更新信息") 
+            t1.start()
+            time.sleep(5)   
+            print(f"开始接收来自{addr}客户端的更新信息，同步{BJ_DIR}文件夹")        
+            t2.start()
+        elif addr == 'New York':
+            bj_previous_state = {}
+            print(f"开始监控{BJ_DIR},向{addr}客户端发送文件夹更新信息") 
+            t1.start()
+            time.sleep(5)   
+            print(f"开始接收来自{addr}客户端的更新信息，同步{NY5_DIR}文件夹")        
+            t2.start()
+
+        
+        t3.start() #如果写try: t3.join()，则t3运行异常后会直接导致主程序失败，不会进入except
